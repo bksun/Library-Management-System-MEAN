2549313714728ce9f8b5e68e8a7ea4bc7a978edf
bksun <sunilsinghmbm@gmail.com>
2018-10-23 20:24:55 +0530

    first commit

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-# LibraryMean5
 
+# LibraryMean5
+  
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.4.
 
 ## Development server
---
+++ models/Book.js
@@ -0,0 +1,15 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var BookSchema = new mongoose.Schema({
+    isbn: String,
+    title: String,
+    author: String,
+    description: String,
+    published_year: String,
+    publisher: String,
+    updated_date: { type: Date, default: Date.now },
+  });
+
+  module.exports = mongoose.model('Book', BookSchema);
